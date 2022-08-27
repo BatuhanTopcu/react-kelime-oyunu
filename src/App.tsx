87518e24d27f5b43588143ecfaf422fc6d0b8066
batu <batuhantopcu17@gmail.com>
2022-08-27 13:04:11 +0300

    project created using cra. eslint setup + rules. tailwind setup. removed boilerplate code from cra.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="w-screen h-screen flex justify-center items-center bg-slate-900 text-white text-5xl">Hey</div>;
+}
+
+export default App;
