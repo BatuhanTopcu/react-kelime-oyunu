348bf855e71a9a8cc9cd0cdf684a50e63d51111d
batu <batuhantopcu17@gmail.com>
2022-08-28 18:21:46 +0300

    valid and invalid names fix

--- src/utils/useNames.ts
+++ src/utils/useNames.ts
@@ -6,16 +6,16 @@ export const useNames = () => {
   const [nameHistory, setNameHistory] = useState<string[]>([]);
   const nameHistoryRef = useRef<string[]>([]);
 
-  const validNames = () => {
-    if (nameHistory.length === 0) return allNames;
+  const getValidNames = () => {
+    if (nameHistoryRef.current.length === 0) return allNames;
     const lastNamesLastLetter = nameHistoryRef.current[nameHistoryRef.current.length - 1].slice(-1);
     return allNames
       .filter((name) => name.toLocaleLowerCase()[0] === lastNamesLastLetter)
       .filter((name) => !nameHistoryRef.current.includes(name.toLocaleLowerCase()));
   };
 
-  const invalidNames = () => {
-    if (nameHistory.length === 0) return [];
+  const getInvalidNames = () => {
+    if (nameHistoryRef.current.length === 0) return [];
     const lastNamesLastLetter = nameHistoryRef.current[nameHistoryRef.current.length - 1].slice(-1);
     return [
       ...allNames.filter((name) => !(name.toLocaleLowerCase()[0] === lastNamesLastLetter)),
@@ -28,21 +28,27 @@ export const useNames = () => {
     const lowerName = name.toLocaleLowerCase();
     const lastWord = nameHistoryRef.current[nameHistoryRef.current.length - 1];
     if (lastWord && lowerName[0] !== lastWord[lastWord.length - 1]) return false;
-    return validNames().includes(lowerName);
+    return getValidNames().includes(lowerName);
   };
 
   const appendNewName = (name: string): boolean => {
     const lowerName = name.toLocaleLowerCase();
-    if (!checkValidName(lowerName)) return false;
     setNameHistory((prev) => [...prev, lowerName]);
+    if (!checkValidName(lowerName)) {
+      return false;
+    }
     nameHistoryRef.current.push(lowerName);
     return true;
   };
 
   const getRandomGuessWithError = (errorPercent: number): string | null => {
-    const error = randomIntFromInterval(0, 100) <= errorPercent;
-    if (error && invalidNames.length > 0) return getRandomFromArray(invalidNames());
-    return getRandomFromArray(validNames());
+    const random = randomIntFromInterval(0, 100);
+    const error = random <= errorPercent;
+    const valids = getValidNames();
+    console.log(valids, random, errorPercent, error);
+    const invalids = getInvalidNames();
+    if (error && invalids.length > 0) return getRandomFromArray(invalids);
+    return getRandomFromArray(getValidNames());
   };
 
   return { appendNewName, nameHistory, getRandomGuessWithError };
